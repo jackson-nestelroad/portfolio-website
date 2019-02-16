bbc8c6b6a0d3e2b7e84fea3e3677dd69485906d2
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-02-16 15:17:14 -0600

    Added comments

--- src/js/Classes/DOM.js
+++ src/js/Classes/DOM.js
@@ -1,10 +1,12 @@
 // This object contains a collection of functions for manipulating the DOM
 
 const DOM = {
+    // Get array of elements by query
     getElements: function(query) {
         return document.querySelectorAll(query);
     },
 
+    // Get first element only by query
     getFirstElement: function(query) {
         return this.getElements(query)[0];
     }
--- src/js/Events/Main.js
+++
@@ -1,8 +0,0 @@
-import DOM from '../Classes/DOM';
-
-const main = DOM.getFirstElement('main');
-
-// Mouse move over animation to change speed of background
-main.addEventListener('mousemove', () => {
-    
-});
