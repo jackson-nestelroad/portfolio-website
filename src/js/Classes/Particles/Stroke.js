f78c1291fca744cfd300fa7b6381d7d12314348c
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-03-21 11:55:24 -0500

    Class for Stroke information.

---
+++ src/js/Classes/Particles/Stroke.js
@@ -0,0 +1,8 @@
+// This class simply restores the width and color for a particle's stroke.
+
+export default class Stroke {
+    constructor(width, color) {
+        this.width = width;
+        this.color = color;
+    }
+}
