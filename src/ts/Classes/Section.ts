c254aa2c0bcde43b6a6d458692ac46772d695e33
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-03-24 11:05:22 -0500

    Class for a single <section> of the DOM.

---
+++ src/ts/Classes/Section.ts
@@ -0,0 +1,21 @@
+// Class for getting information about a <section> tag
+
+import { DOM } from '../Modules/DOM'
+
+export default class Section {
+    constructor(private element: HTMLElement) { }
+
+    public inView(): boolean {
+        let bounding = this.element.getBoundingClientRect();
+        let view = DOM.getViewport();
+
+        return bounding.bottom >= 0 &&
+               bounding.right >= 0 &&
+               bounding.top <= view.height && 
+               bounding.left <= view.width;
+    }
+
+    public getID(): string {
+        return this.element.id;
+    }
+}
