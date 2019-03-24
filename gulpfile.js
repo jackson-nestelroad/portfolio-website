dd5be7d5073bcff5b3cb920a74d3796fca07cd53
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-03-24 10:52:07 -0500

    Set up TypeScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 // Manage preprocessors and transpilers
 
 const gulp = require('gulp');
-const babel = require('gulp-babel');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('./tsconfig.json');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const connect = require('gulp-connect');
+const clean = require('gulp-clean');
 const browserify = require('browserify');
-const babelify = require('babelify');
 const glob = require('glob');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
@@ -30,33 +31,48 @@ gulp.task('sass', () => {
 		.pipe(connect.reload())
 });
 
-// Transpile ES6 with Babel and then Browsify the result
-gulp.task('browserify', () => {
-    return browserify({
-        entries: glob.sync('src/js/**/*.js'),
-        debug: true
-    })
-        .transform('babelify', { presets: ['@babel/preset-env'] })
-        .bundle()
-        .pipe(source('scripts.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init())
-        .pipe(gulp.dest('out/js'))
-        .pipe(connect.reload())
-});
-
 // Watch HTML for changes to auto-reload change
 gulp.task('html', () => {
 	return gulp.src('./*.html')
 	.pipe(connect.reload())
 });
 
+// Clean up the transpiled TypeScript files
+gulp.task('clean', () => {
+	return gulp.src('out/ts', { read: false }).pipe(clean());
+});
+
+// Transpile all TypeScript files into JavaScript
+gulp.task('typescript', () => {
+	return tsProject.src()
+		.pipe(tsProject())
+		.js
+		.pipe(gulp.dest('out/ts'))
+});
+
+// Browserify and merge all JavaScript files together
+gulp.task('browserify', () => {
+	return browserify({
+        entries: glob.sync('out/ts/**/*.js'),
+        debug: true
+    })
+		.bundle()
+		.pipe(source('scripts.js'))
+		.pipe(buffer())
+        .pipe(sourcemaps.init())
+		.pipe(gulp.dest('out/js'))
+		.pipe(connect.reload());
+});
+
 // Watch for changes to run tasks above
 gulp.task('watch', () => {
 	gulp.watch('src/scss/*.+(scss|sass)', gulp.parallel('sass'));
-	gulp.watch('src/js/**/*.js', gulp.parallel('browserify'));
+	gulp.watch('src/ts/**/*.ts', gulp.parallel('compile'));
 	gulp.watch('*.html', gulp.parallel('html'));
 });
 
+// Compile TypeScript to JavaScript
+gulp.task('compile', gulp.series('typescript', 'browserify', 'clean'));
+
 // Run everything
-gulp.task('default', gulp.parallel('browserify', 'sass', 'html', 'connect', 'watch'));
+gulp.task('default', gulp.parallel('compile', 'sass', 'html', 'connect', 'watch'));
