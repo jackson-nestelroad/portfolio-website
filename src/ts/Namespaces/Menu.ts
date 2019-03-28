697bcc890af949a134f3205e0676f926ded7b067
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-03-27 23:13:27 -0500

    Implement Event Dispatcher to Menu.

--- src/ts/Namespaces/Menu.ts
+++ src/ts/Namespaces/Menu.ts
@@ -1,20 +1,37 @@
 // Namespace for Menu data
 
 import { Menu as Elements } from '../Modules/WebPage'
+import { EventCallback, EventDispatcher } from '../Classes/EventDispatcher';
 
 export namespace Menu {
     let open: boolean = false;
     let right: boolean = false;
+    const dispatcher = new EventDispatcher("menuToggle", { open: open }, false);
 
-    export let hamburger: HTMLElement = Elements.Hamburger;
+    export let Hamburger: HTMLElement = Elements.Hamburger;
 
     export function toggle(): void {
         open = !open;
         if(open) {
-            hamburger.classList.add('open');
+            Hamburger.classList.add('open');
         }
         else {
-            hamburger.classList.remove('open');
+            Hamburger.classList.remove('open');
         }
+        dispatcher.disperse();
+    }
+
+    export function move(moveRight: boolean): void {
+        if(right !== moveRight) {
+            right = moveRight;
+        }
+    }
+
+    export const subscribe = (element: HTMLElement, callback: EventCallback): void => {
+        dispatcher.subscribe(element, callback);
+    }
+
+    export const unsubscribe = (element: HTMLElement): void => {
+        dispatcher.unsubscribe(element);
     }
 }
