b478a5aa63c1532c4b62fa0c2087cbbfe1d360ac
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-03-27 23:12:58 -0500

    Getting ready to add menu movement.

--- src/ts/Events/Menu.ts
+++ src/ts/Events/Menu.ts
@@ -1,7 +1,12 @@
 // Toggle the menu on click
 
+import { Sections } from '../Modules/WebPage'
 import { Menu } from '../Namespaces/Menu'
 
-Menu.hamburger.addEventListener('click', () => {
+Menu.Hamburger.addEventListener('click', () => {
     Menu.toggle();
+});
+
+document.addEventListener('scroll', () => {
+    Menu.move(Sections['canvas'].inView());
 });
