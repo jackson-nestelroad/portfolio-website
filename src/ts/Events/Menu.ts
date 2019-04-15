9bcb84be93fedd53027b0386ca778dbdddf73ff0
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-04-15 08:43:35 -0500

    Remove scroll event (for now).

--- src/ts/Events/Menu.ts
+++ src/ts/Events/Menu.ts
@@ -1,12 +1,7 @@
 // Toggle the menu on click
 
-import { Sections } from '../Modules/WebPage'
 import { Navigation } from '../Modules/WebPage'
 
 Navigation.Hamburger.addEventListener('click', () => {
     Navigation.toggle();
-});
-
-document.addEventListener('scroll', () => {
-    // Menu.move(Sections['canvas'].inView());
 });
