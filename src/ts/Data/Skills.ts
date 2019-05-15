116aa90e0f97132c6682b687316786076d83ac46
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-05-15 12:58:15 -0500

    Write all skills data.

---
+++ src/ts/Data/Skills.ts
@@ -0,0 +1,96 @@
+// Data for all Skills
+
+import { SkillData } from '../Classes/Elements/Skill'
+
+export const Skills: Array<SkillData> = [
+    {
+        name: "C++",
+        svg: "cplusplus",
+        color: "#9B023A"
+    },
+    {
+        name: "C#",
+        svg: "csharp",
+        color: "#9B4F97"
+    },
+    {
+        name: "CSS",
+        svg: "css",
+        color: "#3C9CD7"
+    },
+    {
+        name: "Docker",
+        svg: "docker",
+        color: "#22B9EC"
+    },
+    {
+        name: ".NET Core/Framework",
+        svg: "dotnet",
+        color: "#0F76BD"
+    },
+    {
+        name: "Express JS",
+        svg: "express",
+        color: "#3D3D3D"
+    },
+    {
+        name: "Google Cloud Platform",
+        svg: "gcp",
+        color: "#4386FA"
+    },
+    {
+        name: "Gulp",
+        svg: "gulp",
+        color: "#DA4648"
+    },
+    {
+        name: "Heroku",
+        svg: "heroku",
+        color: "#6762A6"
+    },
+    {
+        name: "HTML",
+        svg: "html",
+        color: "#EF652A"
+    },
+    {
+        name: "JavaScript",
+        svg: "javascript",
+        color: "#F0DB4F"
+    },
+    {
+        name: "Kubernets",
+        svg: "kubernetes",
+        color: "#356DE6"
+    },
+    {
+        name: "Node.js",
+        svg: "nodejs",
+        color: "#8CC84B"
+    },
+    {
+        name: "PostgreSQL",
+        svg: "postgresql",
+        color: "#326690"
+    },
+    {
+        name: "React",
+        svg: "react",
+        color: "#00D8FF"
+    },
+    {
+        name: "R Language",
+        svg: "rlang",
+        color: "#2369BC"
+    },
+    {
+        name: "SASS/SCSS",
+        svg: "sass",
+        color: "#CD669A"
+    },
+    {
+        name: "TypeScript",
+        svg: "typescript",
+        color: "#007ACC"
+    }
+];
