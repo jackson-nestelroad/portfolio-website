6c94068b5d55dfccdfcf628a107c264316c08956
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-05-15 12:58:32 -0500

    Write class to dynamically create and draw skills.

---
+++ src/ts/Classes/Elements/Skill.ts
@@ -0,0 +1,78 @@
+// Class for a single skill in the Skill section
+
+import { SVG } from '../../Modules/SVG'
+
+// Format for data for each skill
+export interface SkillData {
+    name: string,
+    svg: string,
+    color: string
+}
+
+// Class to craft an element from SkillData
+export class Skill {
+    private element: HTMLElement = null;
+    public readonly data: SkillData;
+
+    private static HexagonSVG: SVGSVGElement;
+
+    constructor(data: SkillData) {
+        this.data = data;
+    }
+
+    // Creates an element once
+    public createElement(): Promise<HTMLElement> {
+        return new Promise((resolve, reject) => {
+            if(this.element) {
+                resolve(this.element);
+            }
+
+            SVG.loadSVG(`./out/images/Skills/${this.data.svg}`).then(svg => {
+                svg.setAttribute('class', 'icon');
+
+                let parent = document.createElement('li');
+                parent.classList.add('skill');
+                
+                let hexagonContainer = document.createElement('div');
+                hexagonContainer.classList.add('container');
+                hexagonContainer.setAttribute('style', `color: ${this.data.color}`);
+        
+                hexagonContainer.appendChild(svg);
+
+                let hexagon = Skill.HexagonSVG.cloneNode(true);
+                hexagonContainer.appendChild(hexagon);
+        
+                parent.appendChild(hexagonContainer);
+
+                this.element = parent;
+
+                resolve(parent);
+            })
+            .catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    // Initializes static members
+    public static initialize(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            // Resolve immediately if we already have initialized
+            if(Skill.HexagonSVG) {
+                resolve(true);
+            }
+            else {
+                SVG.loadSVG('./out/images/Content/Hexagon').then(element => {
+                    element.setAttribute('class', 'hexagon');
+                    Skill.HexagonSVG = element;
+                    resolve(true);
+                })
+                .catch(err => {
+                    resolve(false);
+                });
+            }
+        });
+    }
+}
+// Initialize now
+Skill.initialize();
