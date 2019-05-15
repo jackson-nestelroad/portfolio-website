6370a3d09e1f4313c6dbf192b7c650d8eddbb78e
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-05-15 12:57:37 -0500

    Function to load SVG from URL or file.

--- src/ts/Modules/SVG.ts
+++ src/ts/Modules/SVG.ts
@@ -3,4 +3,21 @@
 export module SVG {
     export const svgns: string = 'http://www.w3.org/2000/svg';
     export const xlinkns: string = 'http://www.w3.org/1999/xlink';
+
+    // Load an SVG from a URL/file
+    export const loadSVG = (url: string): Promise<SVGSVGElement> => {
+        return new Promise((resolve, reject) => {
+            let request = new XMLHttpRequest();
+            request.open('GET', `${url}.svg`, true);
+            request.onload = () => {
+                let parser = new DOMParser();
+                let parsedDocument: Document = parser.parseFromString(request.responseText, 'image/svg+xml');
+                resolve(parsedDocument.querySelector('svg'));
+            }
+            request.onerror = () => {
+                reject("Failed to read SVG.");
+            }
+            request.send();
+        });
+    }
 }
