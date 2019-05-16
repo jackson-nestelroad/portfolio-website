6d020e6a9e3b871da916cbb413d11328ca9de930
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-05-16 09:25:38 -0500

    Fixed a small typo in about section.

--- src/ts/Data/About.ts
+++ src/ts/Data/About.ts
@@ -1,4 +1,4 @@
 // Flavor text for About section
 
 export const AboutMe: string = 
-"I am an aspiring web developer and software engineering chasing my passion for working with technology and programming at the University of Texas at Dallas. I crave the opportunity to contribute to meaningful projects that employ my current gifts and interests while also shoving me out of my comfort zone to learn new skills. My goal is to maximize every experience as an opportunity for personal, professional, and technical growth.";
+"I am an aspiring web developer and software engineer chasing my passion for working with technology and programming at the University of Texas at Dallas. I crave the opportunity to contribute to meaningful projects that employ my current gifts and interests while also shoving me out of my comfort zone to learn new skills. My goal is to maximize every experience as an opportunity for personal, professional, and technical growth.";
