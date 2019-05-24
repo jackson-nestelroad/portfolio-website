4fd5c30d126533cbf7ce99afb5d8c408fc8a6c02
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-05-23 19:24:57 -0500

    Two descriptions done.

--- src/ts/Data/Qualities.ts
+++ src/ts/Data/Qualities.ts
@@ -6,12 +6,12 @@ export const Qualities: Array<QualityData> = [
     {
         faClass: 'fas fa-history',
         name: 'Efficient',
-        description: 'Text :)'
+        description: 'I consistently bring energy, productivity, organization, and agility to the table as an effective worker and a quick learner.'
     },
     {
         faClass: 'far fa-snowflake',
         name: 'Attentive',
-        description: 'Text.'
+        description: 'To me, every detail matters. I love formulating the big picture just as much as measuring out the tiny details and edge cases.'
     },
     {
         faClass: 'fas fa-feather-alt',
