b9d46c815f4f969e3581645696cc357869b10746
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-06-17 19:56:39 +0100

    Add Jest to skills, because why not?

--- src/ts/Data/Skills.ts
+++ src/ts/Data/Skills.ts
@@ -4,98 +4,103 @@ import { SkillData } from '../Classes/Elements/Skill'
 
 export const Skills: Array<SkillData> = [
     {
-        name: "C++",
-        svg: "cplusplus",
-        color: "#9B023A"
+        name: 'C++',
+        svg: 'cplusplus',
+        color: '#9B023A'
     },
     {
-        name: "C#",
-        svg: "csharp",
-        color: "#9B4F97"
+        name: 'C#',
+        svg: 'csharp',
+        color: '#9B4F97'
     },
     {
-        name: "CSS",
-        svg: "css",
-        color: "#3C9CD7"
+        name: 'CSS',
+        svg: 'css',
+        color: '#3C9CD7'
     },
     {
-        name: "Docker",
-        svg: "docker",
-        color: "#22B9EC"
+        name: 'Docker',
+        svg: 'docker',
+        color: '#22B9EC'
     },
     {
-        name: ".NET Core/Framework",
-        svg: "dotnet",
-        color: "#0F76BD"
+        name: '.NET Core/Framework',
+        svg: 'dotnet',
+        color: '#0F76BD'
     },
     {
-        name: "Express JS",
-        svg: "express",
-        color: "#3D3D3D"
+        name: 'Express JS',
+        svg: 'express',
+        color: '#3D3D3D'
     },
     {
-        name: "Google Cloud Platform",
-        svg: "gcp",
-        color: "#4386FA"
+        name: 'Google Cloud Platform',
+        svg: 'gcp',
+        color: '#4386FA'
     },
     {
-        name: "Gulp",
-        svg: "gulp",
-        color: "#DA4648"
+        name: 'Gulp',
+        svg: 'gulp',
+        color: '#DA4648'
     },
     {
-        name: "Heroku",
-        svg: "heroku",
-        color: "#6762A6"
+        name: 'Heroku',
+        svg: 'heroku',
+        color: '#6762A6'
     },
     {
-        name: "HTML",
-        svg: "html",
-        color: "#EF652A"
+        name: 'HTML',
+        svg: 'html',
+        color: '#EF652A'
     },
     {
-        name: "JavaScript",
-        svg: "javascript",
-        color: "#F0DB4F"
+        name: 'JavaScript',
+        svg: 'javascript',
+        color: '#F0DB4F'
     },
     {
-        name: "Kubernetes",
-        svg: "kubernetes",
-        color: "#356DE6"
+        name: 'Jest',
+        svg: 'jest',
+        color: '#C21325'
     },
     {
-        name: "Node.js",
-        svg: "nodejs",
-        color: "#8CC84B"
+        name: 'Kubernetes',
+        svg: 'kubernetes',
+        color: '#356DE6'
     },
     {
-        name: "PostgreSQL",
-        svg: "postgresql",
-        color: "#326690"
+        name: 'Node.js',
+        svg: 'nodejs',
+        color: '#8CC84B'
     },
     {
-        name: "React",
-        svg: "react",
-        color: "#00D8FF"
+        name: 'PostgreSQL',
+        svg: 'postgresql',
+        color: '#326690'
     },
     {
-        name: "R Language",
-        svg: "rlang",
-        color: "#2369BC"
+        name: 'React',
+        svg: 'react',
+        color: '#00D8FF'
     },
     {
-        name: "SASS/SCSS",
-        svg: "sass",
-        color: "#CD669A"
+        name: 'R Language',
+        svg: 'rlang',
+        color: '#2369BC'
     },
     {
-        name: "TypeScript",
-        svg: "typescript",
-        color: "#007ACC"
+        name: 'SASS/SCSS',
+        svg: 'sass',
+        color: '#CD669A'
     },
     {
-        name: "Vue",
-        svg: "vue",
-        color: "#4FC08D"
+        name: 'TypeScript',
+        svg: 'typescript',
+        color: '#007ACC'
+    },
+    {
+        name: 'Vue',
+        svg: 'vue',
+        color: '#4FC08D'
     }
 ];
