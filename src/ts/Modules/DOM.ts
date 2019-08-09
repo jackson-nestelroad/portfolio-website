ae25bc1c847c9a3208f5a33bcc33bb74950fa2d8
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-08-09 14:11:10 -0500

    A few updated and new DOM functions.

--- src/ts/Modules/DOM.ts
+++ src/ts/Modules/DOM.ts
@@ -14,18 +14,13 @@ export module DOM {
     // Get height and width of viewport
     export function getViewport(): { height: number, width: number } {
         return {
-            height: window.innerHeight || document.documentElement.clientHeight,
-            width: window.innerWidth || document.documentElement.clientWidth
+            height: Math.max(window.innerHeight, document.documentElement.clientHeight),
+            width: Math.max(window.innerWidth, document.documentElement.clientWidth)
         }
     }
 
-    // Scroll to position on the screen
-    export function scrollTo(x: number, y: number): void {
-        window.scrollTo({
-            top: y,
-            left: x,
-            behavior: 'smooth'
-        });
+    export function scrollTo(element: Element): void {
+
     }
 
     export function isIE(): boolean {
@@ -45,5 +40,67 @@ export module DOM {
                 document.addEventListener('DOMContentLoaded', callback);
             }
         });
+    }
+
+    function boundingClientRectToObject(rect: (ClientRect & { x?: number, y?: number }) | DOMRect): object {
+        return {
+            top: rect.top,
+            right: rect.right,
+            bottom: rect.bottom,
+            left: rect.left,
+            width: rect.width,
+            height: rect.height,
+            x: rect.x ? rect.x : 0,
+            y: rect.y ? rect.y : 0
+        };
+    }
+
+    export function onPage(element: Element): boolean {
+        const rect = element.getBoundingClientRect();
+        return !Object.values(boundingClientRectToObject(rect)).every(val => val === 0);
+    }
+
+    export function inView(element: Element, offset: number = 0): boolean {
+        const rect = element.getBoundingClientRect();
+
+        if(Object.values(boundingClientRectToObject(rect)).every(val => val === 0)) {
+            return false;
+        }
+
+        const viewHeight = getViewport().height;
+
+        if(offset <= 1) {
+            offset = viewHeight * offset;
+        }
+
+        return (rect.bottom + offset) >= 0 && (rect.top + offset - viewHeight) < 0;
+        
+    }
+
+    interface FirstAppearanceSettings {
+        timeout?: number,
+        offset?: number
+    }
+    export function onFirstAppearance(element: Element, callback: Function, setting?: FirstAppearanceSettings): void {
+        const timeout = setting ? setting.timeout : 0;
+        const offset = setting ? setting.offset : 0;
+
+        if(inView(element, offset)) {
+            setTimeout(callback, timeout);
+        }
+        else {
+            const eventCallback = (event: Event) => {
+                if(inView(element, offset)) {
+                    setTimeout(callback, timeout);
+                    document.removeEventListener('scroll', eventCallback, {
+                        capture: true
+                    });
+                }
+            }
+            document.addEventListener('scroll', eventCallback, {
+                capture: true,
+                passive: true
+            });
+        }
     }
 }
