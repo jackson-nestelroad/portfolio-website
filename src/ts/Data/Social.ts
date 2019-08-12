3bb2cc5a4f27c5b79177ad3bd47aa4e25fc7c101
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-08-12 15:32:30 -0500

    Social media data.

---
+++ src/ts/Data/Social.ts
@@ -0,0 +1,21 @@
+// List of social media icons in Connect section
+
+import { SocialData } from '../Classes/Elements/Social'
+
+export const Social: SocialData[] = [
+    {
+        name: 'GitHub',
+        faClass: 'fab fa-github',
+        link: 'https://github.com/jackson-nestelroad'
+    },
+    {
+        name: 'LinkedIn',
+        faClass: 'fab fa-linkedin',
+        link: 'https://www.linkedin.com/in/jacksonroad7/'
+    },
+    {
+        name: 'Email',
+        faClass: 'fas fa-envelope',
+        link: 'mailto:jackson@nestelroad.com'
+    }
+];
