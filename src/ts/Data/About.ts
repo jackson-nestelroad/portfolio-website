b4676742392989ce56ce2f31fd4f4414cdb103fa
Jackson Nestelroad <jackson@nestelroad.com>
2022-07-29 07:43:41 -0500

    Missing word

--- src/ts/Data/About.ts
+++ src/ts/Data/About.ts
@@ -1,4 +1,4 @@
 // Flavor text for About section
 
 export const AboutMe: string =
-    "I am a software engineer excited about using technology to empower the world. Over the years, I have gained experience with a wide range technologies, from full-stack web applications to low-level systems. I have an eye for efficiency, organization, and technical details, and I love working within and across teams to design and build things that advance the beautiful, turbulent field of technology."
+    "I am a software engineer excited about using technology to empower the world. Over the years, I have gained experience with a wide range of technologies, from full-stack web applications to low-level systems. I have an eye for efficiency, organization, and technical details, and I love working within and across teams to design and build things that advance the beautiful, turbulent field of technology."
